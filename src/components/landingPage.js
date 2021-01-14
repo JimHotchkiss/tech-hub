b52d3f3ddbbf33d7b75d3b824d66250166040d28
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2021-01-14 13:16:08 -0500

    Added the layout and styling of the landing page.

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -23,10 +23,34 @@ class LandingPage {
     const featureContainer = document.createElement("div")
     featureContainer.setAttribute("class", "feature-container")
     this.features.map((feature) => {
+      console.log(feature)
       const featureDiv = document.createElement("div")
       featureDiv.setAttribute("class", "feature-div")
       featureDiv.setAttribute("id", feature.id)
-      featureDiv.innerText = feature.title
+
+      // Img-Title Div
+      const imgTitleDiv = document.createElement("div")
+      imgTitleDiv.setAttribute("class", "feature-img-title-div")
+
+      // Img div
+      const imgDiv = document.createElement("div")
+      imgDiv.setAttribute("class", "feature-img-div")
+      imgDiv.setAttribute("id", `${feature.id}-img-url`)
+      imgTitleDiv.appendChild(imgDiv)
+
+      // Title Div
+      const titleDiv = document.createElement("div")
+      titleDiv.setAttribute("class", "feature-title-div")
+      titleDiv.innerText = feature.title
+      imgTitleDiv.appendChild(titleDiv)
+
+      // Description Div
+      const descriptionDiv = document.createElement("div")
+      descriptionDiv.setAttribute("class", "feature-description-div")
+      descriptionDiv.innerText = feature.description
+
+      featureDiv.appendChild(imgTitleDiv)
+      featureDiv.appendChild(descriptionDiv)
       featureContainer.appendChild(featureDiv)
       root.appendChild(featureContainer)
     })
